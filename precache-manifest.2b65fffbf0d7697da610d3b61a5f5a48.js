eb78463b2b2b48b0cf97e30821e8ec73d6d4705e
OrpazL <orpaz.lazar@gmail.com>
2018-12-25 11:33:58 +0200

    Updates

---
+++ precache-manifest.2b65fffbf0d7697da610d3b61a5f5a48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "22ffaae791430465aaaa",
+    "url": "/mister-bitcoin/static/css/main.1c173858.chunk.css"
+  },
+  {
+    "revision": "22ffaae791430465aaaa",
+    "url": "/mister-bitcoin/static/js/main.22ffaae7.chunk.js"
+  },
+  {
+    "revision": "8c901edd90637da83256",
+    "url": "/mister-bitcoin/static/js/1.8c901edd.chunk.js"
+  },
+  {
+    "revision": "e0b16d0559dbb2c784f3",
+    "url": "/mister-bitcoin/static/js/runtime~main.e0b16d05.js"
+  },
+  {
+    "revision": "9929554d8b92be131e99c3bbb9802d51",
+    "url": "/mister-bitcoin/static/media/meeseeks.9929554d.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mister-bitcoin/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7ca12718e344ad9fddab9abfc126d3e2",
+    "url": "/mister-bitcoin/index.html"
+  }
+];
